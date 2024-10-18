9d6955115f207369307db30d04924569098ccefd
lortizh <lortizhernandez83@gmail.com>
2024-10-18 15:05:32 -0500

    Actualizacion package y generacion webpackconfigjson

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function(env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // Custom modifications to Webpack config can go here
+  return config;
+};
